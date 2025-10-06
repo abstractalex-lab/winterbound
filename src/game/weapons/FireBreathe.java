23202615d3fef09c8b84a691c90a909c1567ddb2
Naivee77 <sjin0028@student.monash.edu>
2025-10-06 20:26:41 +1100

    req5: -create new intrinsic weapon FireBreathe for dragon in FireState

---
+++ src/game/weapons/FireBreathe.java
@@ -0,0 +1,50 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.capabilities.Burning;
+import game.grounds.Fire;
+import game.interfaces.Flammable;
+
+import java.util.List;
+import java.util.Random;
+
+public class FireBreathe extends IntrinsicWeapon {
+
+    public FireBreathe() {
+        super(50, "breathes fire", 80, "breathe fire");
+    }
+
+    @Override
+    public String attack(Actor attacker, Actor target, GameMap map) {
+
+        Random rand = new Random();
+
+        List<Location> nearbyLocations = map.locationOf(target).getNearbyLocations(2);
+        for(Location nearbyLocation: nearbyLocations){
+            nearbyLocation.setGround(new Fire());
+            Flammable flammable = nearbyLocation.getActorAs(Flammable.class);
+            if(flammable != null){
+                nearbyLocation.getActor().addStatus(new Burning(flammable, 5, 5));
+            }
+        }
+
+        if (!(rand.nextInt(100) <= this.hitRate)) {
+            return attacker + " misses " + target + ".";
+        }
+        target.hurt(damage);
+
+        Location targetLocation = map.locationOf(target);
+        Flammable targetFlammable = targetLocation.getActorAs(Flammable.class);
+        if (targetFlammable != null) {
+            target.addStatus(new Burning(targetFlammable, 5, 5));
+        }
+
+        return String.format(
+                "%s breathes a wave of fire at %s for %d damage! Flames spread to nearby tiles!",
+                attacker, target, damage
+        );
+    }
+}

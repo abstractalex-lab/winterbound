35ec6f8e978487a19a1c7dc3647c0ebfe8ce664e
Alex Bui <abui0012@student.monash.edu>
2025-10-08 09:08:34 +1100

    Create new file and interface - Added Burning for to be interacted with player - Added Flammable to be used on burning ground for teleportation

---
+++ src/game/capabilities/Burning.java
@@ -0,0 +1,40 @@
+package game.capabilities;
+
+import edu.monash.fit2099.engine.GameEntity;
+import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Location;
+import game.interfaces.Flammable;
+
+public class Burning implements Status {
+
+    private int duration;
+    private int damage;
+    private final Flammable flammable;
+
+    public Burning(Flammable flammable, int duration, int damage) {
+        this.flammable = flammable;
+        this.duration = duration;
+        this.damage = damage;
+    }
+
+    @Override
+    public void tickStatus(GameEntity gameEntity, Location location) {
+        Display display = new Display();
+        if (flammable != null) {
+            display.println(flammable.burn(damage));
+            duration--;
+        }
+    }
+
+    @Override
+    public boolean isStatusActive() {
+        return duration == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Burnable";
+    }
+
+}

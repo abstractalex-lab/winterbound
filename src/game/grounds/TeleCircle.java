ad526cd75020622ca22a4483f28b28ffcecd239f
Alex Bui <abui0012@student.monash.edu>
2025-10-08 08:46:54 +1100

    Update teleportation tools - Created TeleCircle and TeleDoor files to allow for use interaction of teleporting

---
+++ src/game/grounds/TeleCircle.java
@@ -0,0 +1,12 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+/**
+ * Simple teleportation circle ground (symbol 'O').
+ */
+public class TeleCircle extends Ground {
+    public TeleCircle() {
+        super('O', "TeleCircle");
+    }
+}
---
+++ src/game/grounds/TeleDoor.java
@@ -0,0 +1,9 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.positions.Ground;
+
+public class TeleDoor extends Ground {
+    public TeleDoor() {
+        super('#', "TeleDoor");
+    }
+}

f4db3e0ec2ca546eef2eb61cc8f0760445dfaab9
Alex Bui <abui0012@student.monash.edu>
2025-10-08 08:52:58 +1100

    Update maps content - Updated Earth to have new icon representation in the map for future interactions - Added new content for Plains map

---
+++ src/game/items/TeleportCube.java
@@ -0,0 +1,13 @@
+package game.items;
+
+import edu.monash.fit2099.engine.items.Item;
+
+/**
+ * <h2>TeleportCube</h2>
+ * A portable teleportation cube that will be used for REQ1 teleporting.
+ */
+public class TeleportCube extends Item {
+    public TeleportCube() {
+        super("Teleport Cube", '□', true);
+    }
+}
--- src/game/worlds/Earth.java
+++ src/game/worlds/Earth.java
@@ -9,7 +9,10 @@ import game.actors.animals.Animal;
 import game.actors.animals.Bear;
 import game.actors.animals.Deer;
 import game.actors.animals.Wolf;
+import game.grounds.Dirt;
 import game.grounds.Snow;
+import game.grounds.TeleCircle;
+import game.grounds.TeleDoor;
 import game.grounds.plants.AppleTree;
 import game.grounds.plants.HazelnutTree;
 import game.grounds.plants.YewBerryTree;
@@ -28,6 +31,10 @@ public class Earth extends World {
         groundCreator.registerGround('T', AppleTree::new);
         groundCreator.registerGround('A', HazelnutTree::new);
         groundCreator.registerGround('Y', YewBerryTree::new);
+        groundCreator.registerGround('#', TeleDoor::new);
+        groundCreator.registerGround('O', TeleCircle::new);
+        groundCreator.registerGround('^', FireGround::new);
+        groundCreator.registerGround('+', Dirt::new);
 
         List<String> map = Arrays.asList(
                 ".T....T.................................",

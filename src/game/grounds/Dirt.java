ad17f85f89d326c62a2b0e078610bf6bb2299869
Alex Bui <abui0012@student.monash.edu>
2025-10-08 08:39:14 +1100

    Update Dirt file content - Added the file content with displayChar and name type

--- src/game/grounds/Dirt.java
+++ src/game/grounds/Dirt.java
@@ -6,8 +6,8 @@ import edu.monash.fit2099.engine.positions.Ground;
  * A class representing snow on the ground.
  * @author Alex Bui
  */
-public class Snow extends Ground {
-    public Snow() {
-        super('', "Snow");
+public class Dirt extends Ground {
+    public Dirt() {
+        super('+', "Dirt");
     }
 }

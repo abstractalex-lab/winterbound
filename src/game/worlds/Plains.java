22ad99789453d325a2c7772165e464951131465b
Alex Bui <abui0012@student.monash.edu>
2025-10-08 08:23:52 +1100

    Added Plains file map - Added the new map file Plains to support the new teleportation location

---
+++ src/game/worlds/Plains.java
@@ -0,0 +1,19 @@
+package game.worlds;
+
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.World;
+
+public class Plains extends World {
+    /**
+     * Constructor.
+     *
+     * @param display the Display that will display this World.
+     */
+    public Plains(Display display) {
+        super(display);
+    }
+
+    public void constructWorld() throws Exception {
+
+    }
+}

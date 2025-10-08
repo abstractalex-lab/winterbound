174c383f956d13aadff1fd26f130c9b4e2782044
Alex Bui <abui0012@student.monash.edu>
2025-10-09 08:06:46 +1100

    Update FireGround with content implementation - Added burning mechanism and hp deduction for burning fire of ground

--- src/game/grounds/FireGround.java
+++ src/game/grounds/FireGround.java
@@ -7,15 +7,31 @@ import game.interfaces.Flammable;
 
 public class FireGround extends Ground implements Flammable {
 
-    private int lifetime = 3; // lasts 3 turns
+    private int burnTime = 3; // lasts 3 turns
     private final int damagePerTurn = 5;
 
     public FireGround() {
         super('^', "Fire");
     }
 
+    @Override
     public void tick(Location location) {
+        Actor actor = location.getActor();
 
+        if (actor != null) {
+            actor.hurt(damagePerTurn);
+            System.out.println(actor + " is burned for " + damagePerTurn + " damage");
+        }
+
+        burnTime--;
+        if (burnTime <= 0) {
+            location.setGround(new Dirt());
+            System.out.println("Fire burned out and became Dirt at " + location.x() + "," + location.y());
+        }
     }
 
+    @Override
+    public String burn(int damage) {
+        return "The fire burns fiercely for " + damage + " damage per turn!";
+    }
 }

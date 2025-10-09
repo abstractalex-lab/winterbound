209aa85cef35d8dcbd35e18530a9977620daff31
Alex Bui <abui0012@student.monash.edu>
2025-10-09 20:53:50 +1100

    Update locations' spawning mechanisms - Simplified spawning location for each Teleportation tool

--- src/game/worlds/Earth.java
+++ src/game/worlds/Earth.java
@@ -5,12 +5,10 @@ import edu.monash.fit2099.engine.positions.DefaultGroundCreator;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
 import game.actors.Player;
-import game.actors.animals.Animal;
-import game.actors.animals.Bear;
-import game.actors.animals.Deer;
-import game.actors.animals.Wolf;
+import game.actors.animals.*;
 import game.grounds.*;
 import game.grounds.plants.*;
+import game.items.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -62,23 +60,49 @@ public class Earth extends World {
         GameMap plainsMap = plainsWorld.constructWorld();
         this.addGameMap(plainsMap);
 
-        var forestDoorLoc = gameMap.at(18, 2);
+        //door locations across maps
+        var forestDoorLoc = gameMap.at(10, 2);
         if (!(forestDoorLoc.getGround() instanceof TeleportDoor)) {
             forestDoorLoc.setGround(new TeleportDoor());
         }
-        var plainsDoorLoc = plainsMap.at(10, 1);
+
+        var plainsDoorLoc = plainsMap.at(9, 3);
         if (!(plainsDoorLoc.getGround() instanceof TeleportDoor)) {
             plainsDoorLoc.setGround(new TeleportDoor());
         }
 
-        // Link both directions
+        //circle locations across maps
+        var forestCircleLoc = gameMap.at(5, 3);
+        if (!(forestCircleLoc.getGround() instanceof TeleportCircle)) {
+            forestCircleLoc.setGround(new TeleportCircle());
+        }
+
+        var plainsCircleLoc = plainsMap.at(2, 3);
+        if (!(plainsCircleLoc.getGround() instanceof TeleportCircle)) {
+            plainsCircleLoc.setGround(new TeleportCircle());
+        }
+
+        //explicit of arrival at only 1,1
+        var plainsArrival = plainsMap.at(1, 1);
+        var forestArrival = gameMap.at(1, 1);
+        var plainsCircleArrival = plainsMap.at(1, 1);
+        var forestCircleArrival = gameMap.at(1, 1);
+
+        //link teleport portals between Forest and Plains
         TeleportDoor forestDoor = (TeleportDoor) forestDoorLoc.getGround();
         TeleportDoor plainsDoor = (TeleportDoor) plainsDoorLoc.getGround();
-        forestDoor.addDestination(plainsMap, plainsDoorLoc);
-        plainsDoor.addDestination(gameMap,  forestDoorLoc);
+        TeleportCircle forestCircle = (TeleportCircle) forestCircleLoc.getGround();
+        TeleportCircle plainsCircle = (TeleportCircle) plainsCircleLoc.getGround();
+        forestDoor.addDestination(plainsMap, plainsArrival);
+        plainsDoor.addDestination(gameMap, forestArrival);
+        forestCircle.addDestination(plainsMap, plainsCircleArrival);
+        plainsCircle.addDestination(gameMap, forestCircleArrival);
 
-        // Place cube on Forest ground to pick up
-        game.items.TeleportCube cube = new game.items.TeleportCube();
+        //cube location
+        TeleportCube cube = new TeleportCube();
         gameMap.at(6, 1).addItem(cube);
+
+        cube.addEndpoint(gameMap.at(1, 1));
+        cube.addEndpoint(plainsMap.at(1, 1));
     }
 }
--- src/game/worlds/Plains.java
+++ src/game/worlds/Plains.java
@@ -10,8 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * <h2>Plains Map</h2>
- * A second map introduced in REQ1, designed for teleportation and
+ * Plains map. A second map introduced in REQ1, designed for teleportation and
  * inter-map travel testing. Contains grasslands and teleportation grounds.
  */
 public class Plains extends World {
@@ -29,7 +28,7 @@ public class Plains extends World {
      * Builds the "Plains" map and populates its base elements.
      * This map will later be linked with the "Earth" map via TeleDoors and TeleCircles.
      *
-     * @return
+     * @return The map of the Plains
      */
     public GameMap constructWorld() throws Exception {
         DefaultGroundCreator groundCreator = new DefaultGroundCreator();
@@ -43,9 +42,9 @@ public class Plains extends World {
                 ".........................",
                 ".........................",
                 ".........................",
-                "..O......#..............",
                 ".........................",
-                "........................",
+                ".........................",
+                ".........................",
                 ".........................",
                 "........................."
         );

303c825c210a05ee492d048c4a7a5acbcea7f9f5
Timothy Lim <tlim0034@student.monash.edu>
2025-10-28 23:00:09 +1100

    New Spawning Ground - Swamp
    - If there is an actor within its surroundings, swamps have a 50% chance to spawn an animal.
    - All animals spawned from a swamp will be poisoned for ten turns, starting from when they’re spawned, dealing five damage per turn

---
+++ src/main/java/game/grounds/Swamp.java
@@ -0,0 +1,50 @@
+// file: game/grounds/Swamp.java
+package game.grounds;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * Swamp spawner ('~'):
+ * - Only attempts when an actor exists in surrounding exits.
+ * - 50% chance each tick to spawn.
+ * - All animals spawned from a swamp are poisoned for 10 turns (5 dmg/turn).
+ */
+public class Swamp extends SpawningGround {
+    private final List<Supplier<? extends Actor>> spawnables;
+
+    public Swamp() { this(new ArrayList<>()); }
+
+    public Swamp(List<Supplier<? extends Actor>> spawnables) {
+        super('~', "Swamp");
+        this.spawnables = new ArrayList<>(Objects.requireNonNull(spawnables));
+    }
+
+    @Override protected double spawnChance() { return 0.5; }
+
+    @Override protected int spawnCooldownTicks() { return 1; }
+
+    @Override
+    protected boolean canAttempt(Location here) {
+        for (Exit e : here.getExits()) {
+            if (e.getDestination().containsAnActor()) return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected List<Supplier<? extends Actor>> spawnTable() {
+        // Wrap suppliers to apply swamp-born poison to spawned animals.
+        List<Supplier<? extends Actor>> wrapped = new ArrayList<>();
+        for (Supplier<? extends Actor> s : spawnables) {
+            wrapped.add(() -> PostSpawnEffects.withSwampPoison(s.get()));
+        }
+        return wrapped;
+    }
+}

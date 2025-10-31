1de75df1b57b0c0c317d58214c5112d115cb99e6
Timothy Lim <tlim0034@student.monash.edu>
2025-10-31 21:51:16 +1100

    feat(crocodile): make croc tameable with unique post-tame behaviours
    -Implements Tameable on Crocodile
    
    -Adds tame flow (stance flip, success rate) and fedBy(...)
    
    -Sets post-tame priorities: Protect(1) → Follow(2) → Detect(3)

--- src/main/java/game/actors/animals/Crocodile.java
+++ src/main/java/game/actors/animals/Crocodile.java
@@ -1,16 +1,22 @@
 // file: game/actors/animals/Crocodile.java
 package game.actors.animals;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.Location;
+import game.behaviours.DetectBehaviour;
+import game.behaviours.FollowBehaviour;
+import game.behaviours.ProtectBehaviour;
 import game.behaviours.WanderBehaviour;
+import game.capabilities.Stance;
 import game.grounds.PostSpawnEffects;
+import game.interfaces.Tameable;
 import game.statuses.Poisoned;
 import game.weapons.CrocBite;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Crocodile extends Animal {
+public class Crocodile extends PredatorAnimal implements Tameable {
     public Crocodile() {
         super("Crocodile", '<', 300,55);
         // Animal already has WanderBehaviour(999) by default in your code,
@@ -24,5 +30,24 @@ public class Crocodile extends Animal {
         PostSpawnEffects.onCrocodileSpawn(this, origin, rng);
         }
 
+
+
+    @Override
+    public String tamedBy(Actor actor) {
+        if (!this.isConscious()) return "";
+        // flip stances
+        enableAbility(Stance.TAMED);
+        disableAbility(Stance.HOSTILE);
+
+        // Croc’s unique post-tame role:
+        // 1: Protect owner first (bodyguard focus),
+        // 2: Follow second (stick close),
+        // 3: Detect third (useful, but lower priority than guarding).
+        addBehaviour(1, new ProtectBehaviour());
+        addBehaviour(2, new FollowBehaviour(actor));
+        addBehaviour(3, new DetectBehaviour());
+
+        return " ,which is successfully tamed by " + actor;
+    }
 
 }

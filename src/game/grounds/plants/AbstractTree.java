9abc8e220d54c16c3dd8cdc078beca2d805a76b7
Alex Bui <abui0012@student.monash.edu>
2025-11-01 18:37:24 +1100

    Update AbstractTree file - Added methods for the rest of the file's content

--- src/game/grounds/plants/AbstractTree.java
+++ src/game/grounds/plants/AbstractTree.java
@@ -1,5 +1,6 @@
 package game.grounds.plants;
 
+import edu.monash.fit2099.engine.positions.Location;
 import game.interfaces.GrowthStage;
 import game.interfaces.GrowthBehaviour;
 
@@ -15,8 +16,8 @@ public abstract class AbstractTree extends Tree {
     /**
      * Constructor for an AbstractTree.
      *
-     * @param displayChar  character to display for this type of tree
-     * @param name         the name of the tree
+     * @param displayChar character to display for this type of tree
+     * @param name        the name of the tree
      * @param initialStage the starting growth stage
      * @param behaviour    the environment-dependent behaviour
      */
@@ -25,4 +26,46 @@ public abstract class AbstractTree extends Tree {
         this.stage = initialStage;
         this.growthBehaviour = behaviour;
     }
+
+    /**
+     * Called each turn to handle growth and fruit production.
+     *
+     * @param location the location of the tree on the map
+     */
+    @Override
+    public void tick(Location location) {
+        stage.grow(this, location);
+        fruitTimer++;
+
+        if (stage.canProduceFruit()
+                && fruitTimer >= stage.getFruitInterval() + growthBehaviour.getFruitIntervalModifier()) {
+            produceFruit(location);
+            fruitTimer = 0;
+        }
+    }
+
+    /**
+     * Updates the tree's growth stage.
+     *
+     * @param nextStage the next growth stage of the tree
+     */
+    public void setStage(GrowthStage nextStage) {
+        this.stage = nextStage;
+    }
+
+    /**
+     * Returns the current environment behaviour.
+     *
+     * @return the GrowthBehaviour instance
+     */
+    public GrowthBehaviour getGrowthBehaviour() {
+        return growthBehaviour;
+    }
+
+    /**
+     * Defines how the specific tree produces fruit.
+     *
+     * @param location the location of the tree on the map
+     */
+    protected abstract void produceFruit(Location location);
 }

f6032542a2059f5b070b27dc8339ec989aa4a5d6
Alex Bui <abui0012@student.monash.edu>
2025-11-01 18:24:03 +1100

    Update Growth interface - Created GrowthBehaviour to defines the environment strategy rules for Tree - Updated GrowthStage content

--- src/game/interfaces/GrowthStage.java
+++ src/game/interfaces/GrowthStage.java
@@ -1,4 +1,23 @@
 package game.interfaces;
 
+import edu.monash.fit2099.engine.positions.Location;
+import game.grounds.plants.Tree;
+
+/**
+ * Represents the growth stage of a tree.
+ * Each stage determines when to transition and if it can produce fruit.
+ */
 public interface GrowthStage {
+
+    void grow(Tree tree, Location location);
+
+    /**
+     * @return true if the plant can currently produce fruit.
+     */
+    boolean canProduceFruit();
+
+    /**
+     * @return number of turns between fruit productions for this stage.
+     */
+    int getFruitInterval();
 }

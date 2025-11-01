9c8c18a9b7f92f87adac570c6550889d7d7c486d
Alex Bui <abui0012@student.monash.edu>
2025-11-01 18:32:37 +1100

    Update GrowthBehaviour file - Added interface content for GrowthInterface file

--- src/game/interfaces/GrowthBehaviour.java
+++ src/game/interfaces/GrowthBehaviour.java
@@ -1,4 +1,21 @@
 package game.interfaces;
 
+/**
+ * Defines environment-specific growth and fruiting rules for trees.
+ * Implemented using the Strategy pattern.
+ */
 public interface GrowthBehaviour {
+
+    /**
+     * Number of turns required for a sprout to become a sapling.
+     */
+    int getSaplingGrowthTurns();
+
+    /**
+     * Number of turns required for a sapling to become mature.
+     * */
+    int getTreeGrowthTurns();
+
+    /** Additional modifier applied to fruit production intervals. */
+    int getFruitIntervalModifier();
 }
